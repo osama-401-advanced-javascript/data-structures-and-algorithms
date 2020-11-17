d8b4bba4228429e823d9a22c7a2149e681a87a14
Othabteh <o.thabteh@gmail.com>
2020-11-17 07:00:22 +0200

    ch27

---
+++ challenges/mergeSort/mergeSort.js
@@ -0,0 +1,38 @@
+function mergeSort(arr) {
+  let n = arr.length;
+
+  if (n > 1) {
+    let mid = Math.floor(n / 2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid);
+    mergeSort(left);
+    mergeSort(right);
+    merge(left, right, arr);
+  }
+  return arr;
+}
+function merge(left, right, arr) {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      arr[k] = left[i];
+      i++;
+    } else {
+      arr[k] = right[j];
+      j++;
+    }
+    k++;
+  }
+  while (i < left.length) {
+    arr[k++] = left[i++];
+  }
+  while (j < right.length) {
+    arr[k++] = right[j++];
+  }
+}
+
+// mergeSort([8, 4, 23, 42, 16, 15]);
+
+module.exports = mergeSort;
