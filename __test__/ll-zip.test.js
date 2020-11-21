02ae71e9b413dadb7c2a58e5a294112abdd1301a
Othabteh <o.thabteh@gmail.com>
2020-11-21 14:56:28 +0200

    fix

--- __test__/ll-zip.test.js
+++ __test__/ll-zip.test.js
@@ -4,24 +4,20 @@
 const LinkedList = require('../challenges/Data-Structures/linkedList/linked-list.js');
 const zipLists = require('../challenges/ll_zip/ll-zip.js');
 
-
-
 describe('zip two lists together function', () => {
-    it('should zip Linked Lists together when the list lengths are equal', () => {
-  
-      const list1 = new LinkedList();
-      const list2 = new LinkedList();
-  
-  
-      list1.insert(2);
-      list1.insert(3);
-      list1.insert(1);
-  
-      list2.insert(4);
-      list2.insert(9);
-      list2.insert(5);
-  
-      const zipper = zipLists(list1, list2);
-      expect(zipper.toString()).toBe('{ 1 } -> { 5 } -> { 3 } -> { 9 } -> { 2 } -> { 4 } -> NULL');
-    })
+  it('should zip Linked Lists together when the list lengths are equal', () => {
+    const list1 = new LinkedList();
+    const list2 = new LinkedList();
+
+    list1.insert(2);
+    list1.insert(3);
+    list1.insert(1);
+
+    list2.insert(4);
+    list2.insert(9);
+    list2.insert(5);
+
+    const zipper = zipLists(list1, list2);
+    expect(zipper.toString()).toBe('{ 1 } -> { 5 } -> { 3 } -> { 9 } -> { 2 } -> { 4 } -> NULL');
+  });
 });
